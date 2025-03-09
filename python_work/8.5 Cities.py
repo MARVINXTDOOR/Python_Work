b98811943369a553e4c7c4cdc0690b2a71a587e1
Marvin Pearson <marvinbpearson@students.abtech.edu>
2025-03-09 12:06:55 -0700

    Add files via upload

---
+++ python_work/8.5 Cities.py
@@ -0,0 +1,8 @@
+#8.5 Cities
+def describe_city(city, country):
+    """Display information about a city and its country."""
+    print(f"\n {city} is in {country}")
+
+describe_city('Reykjavik', 'Iceland')
+describe_city('Akureyri', 'Iceland')
+describe_city('Guadalajara', 'Mexico')
